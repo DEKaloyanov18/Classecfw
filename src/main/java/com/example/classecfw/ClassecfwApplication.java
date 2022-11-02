93dac628d5d0dc5519be3eef72d3f2cbf532e110
DEKaloyanov18 <DEKaloyanov18@codingburgas.bg>
2022-11-02 14:56:54 +0200

    Initial commit

---
+++ src/main/java/com/example/classecfw/ClassecfwApplication.java
@@ -0,0 +1,13 @@
+package com.example.classecfw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassecfwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassecfwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/classecfw/ClassecfwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classecfw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassecfwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
